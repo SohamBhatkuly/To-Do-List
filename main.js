30ec5f40e2c4d1c0a94eddef0fdd14eb60cfb368
Soham <sohambhatkuly@gmail.com>
2025-02-07 19:09:43 +0530

    modal changes

--- main.js
+++ main.js
@@ -7,8 +7,15 @@ addModal.addEventListener('click', () => {
      modal.style.display = 'block';
 })
 
+
+
 closeModal.addEventListener('click', () => {
     modal.style.display = 'none';
 })
 
 
+flatpickr("input[type=datetime-local", {
+      enableTime: true,
+    dateFormat: "d-m-Y H:i",
+    minDate: "today",
+});
