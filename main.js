1d70404e338adaa4b8511d088c9f073fd05ba2db
Soham <sohambhatkuly@gmail.com>
2025-02-09 12:41:31 +0530

    Description modal

--- main.js
+++ main.js
@@ -3,7 +3,12 @@ let modal = document.querySelector('.modal');
 let closeModal = document.querySelector('.close');
 // console.log(modal)
 let tasks = JSON.parse(localStorage.getItem("data")) || [];
+let closePopup = document.querySelector('.close-popup')
+let body = document.querySelector('body');
+let tiles = document.querySelector('.tiles')
+let popup = document.querySelector('.popup');
 display();
+
 let saveButton = document.querySelector('.save-button');
 saveButton.addEventListener('click', () => {
     addTasks();
@@ -11,8 +16,10 @@ saveButton.addEventListener('click', () => {
     display();
 })
 
-let body = document.querySelector('body');
-let tiles=document.querySelector('.tiles')
+closePopup.addEventListener('click', () => {
+    popup.style.display = 'none';
+})
+
 
 addModal.addEventListener('click', () => {
     modal.style.display = 'block';
@@ -31,9 +38,63 @@ flatpickr("input[type=datetime-local", {
 });
 
 
-function tileDetail(e) {
-    alert(e);
+function popupDisplay(title, date, time, description){
+    popup.style.display = 'block';
+    requestAnimationFrame(() => {
+        displayPopup(title, date, time, description);
+    });
+
 }
+
+function displayPopup(title, date, time, description) {
+    console.log(title, date, time, description);
+    popup.innerHTML = ``;
+    let fragment1 = document.createDocumentFragment();
+    fragment1.innerHTML = ``;
+    let div2 = document.createElement('div');
+    div2.innerHTML = " ";
+    div2.innerHTML = `
+         <button class="close-popup">
+                    <i class="bi bi-x-lg"></i>
+            </button>
+        <div class="popup-title">${title}</div>
+            <div class="popup-date">${date}</div>
+            <div class="popup-time">${time}</div>
+            <div class="popup-description">${description}</div>
+    `;
+    fragment1.appendChild(div2);
+    requestAnimationFrame(() => {
+        popup.appendChild(fragment1);
+    })
+}
+
+
+
+// function tileDetail(e) {
+//     alert(e);
+//     let div1 = document.createElement('div'); 
+//     div1.setAttribute('class', 'modal');
+//     div1.innerHTML = `
+//     <div class="display-modal">
+//                 <button class="close">
+//                     <i class="bi bi-x-lg"></i>
+//                 </button>
+//                 <form class="form">
+//                     <div class="title">
+//                         <h4>Title</h4>
+//                         <input type="text" class="title-value">
+//                     </div>
+//                     <div class="description">
+//                         <h4>Description</h4>
+//                         <textarea name="description-text" class="description-text" maxlength="200"></textarea>
+//                     </div>
+//                     <div class="date">
+//                         <h4>Date</h4>
+//                         <input type="datetime-local" class="date-time">
+//                     </div>
+//                 </form>`;
+//     body.appendChild(div1);
+// }
 
 
 
@@ -46,7 +107,7 @@ function display() {
         return d1 - d2;
     })
     // console.log(tasks);
-
+        let fragment = document.createDocumentFragment();
     let taskDisplay = document.querySelector('.tasks');
     taskDisplay.style.display = "grid";
     taskDisplay.style.gridTemplateColumns = "repeat(5, 1fr)";
@@ -65,10 +126,10 @@ function display() {
         // entry.style.overflow = "auto";
         entry.style.display = "flex";
         entry.style.paddingBottom = "1rem";
-   
+
 
         entry.innerHTML = `
-        <div class="tiles" onclick="tileDetail('${id}')">
+        <div class="tiles"  onclick="popupDisplay('${title}', '${dt}', '${tm[0]}', '${description}')"">
             <div class="tile-title">${title}</div>
 
             <div class="tile-date">${dt}</div>
@@ -76,7 +137,11 @@ function display() {
             <div class="tile-time">${tm[0]}</div>
         </div>
         `;
-        taskDisplay.appendChild(entry);
+        fragment.appendChild(entry);
+    });
+
+    requestAnimationFrame(() => {
+        taskDisplay.appendChild(fragment); 
     });
 }
 
