008a1a438ba162936fe51bde239ce0db6f052d73
Soham <sohambhatkuly@gmail.com>
2025-02-09 16:40:20 +0530

    Modal Display Changes

--- main.js
+++ main.js
@@ -7,94 +7,91 @@ let closePopup = document.querySelector('.close-popup')
 let body = document.querySelector('body');
 let tiles = document.querySelector('.tiles')
 let popup = document.querySelector('.popup');
+    let taskDisplay = document.querySelector('.tasks');
+
+
+
+
 display();
 
 let saveButton = document.querySelector('.save-button');
 saveButton.addEventListener('click', () => {
     addTasks();
     modal.style.display = 'none';
+      document.body.style.position = '';
+    document.body.style.width = '';
+    //    document.body.style.overflowY = "auto";
     display();
 })
-
-closePopup.addEventListener('click', () => {
-    popup.style.display = 'none';
-})
-
-
-addModal.addEventListener('click', () => {
-    modal.style.display = 'block';
-})
-
-closeModal.addEventListener('click', () => {
-    modal.style.display = 'none';
-    display();
-})
-
-
-flatpickr("input[type=datetime-local", {
-      enableTime: true,
-    dateFormat: "Y-m-d H:i",
-    minDate: "today",
-});
 
 
 function popupDisplay(title, date, time, description){
     popup.style.display = 'block';
+    //   document.body.style.position = "fixed";
+    // document.body.style.width = `100%`;
+
     requestAnimationFrame(() => {
         displayPopup(title, date, time, description);
     });
 
 }
 
+closePopup.addEventListener('click', () => {
+    popup.style.display = 'none';
+    //       document.body.style.position = '';
+    // document.body.style.width = '';
+    // document.body.style.overflowY = "auto";
+})
+
+
+addModal.addEventListener('click', () => {
+    modal.style.display = 'block';
+    document.body.style.position = "fixed";
+    document.body.style.width = `100%`;
+    // const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+    // document.body.style.paddingRight=`${scrollbarWidth}px`
+})
+
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+      document.body.style.position = '';
+    document.body.style.width = '';
+    //   document.body.style.overflowY = "auto";
+})
+
+
+flatpickr("input[type=datetime-local", {
+    enableTime: true,
+    dateFormat: "Y-m-d H:i",
+    minDate: "today",
+
+// onOpen: () => {
+//   document.body.classList.add("no-scroll");
+// },
+
+// onClose: () => {
+//   document.body.classList.remove("no-scroll");
+    // }
+    // onValueUpdate: function (selectedDates, dateStr, instance) {
+
+    // }
+
+});
+
+
+
 function displayPopup(title, date, time, description) {
-    console.log(title, date, time, description);
-    popup.innerHTML = ``;
-    let fragment1 = document.createDocumentFragment();
-    fragment1.innerHTML = ``;
-    let div2 = document.createElement('div');
-    div2.innerHTML = " ";
-    div2.innerHTML = `
-         <button class="close-popup">
-                    <i class="bi bi-x-lg"></i>
-            </button>
+   title =  title.charAt(0).toUpperCase() + title.slice(1);
+const detail = document.querySelector('.details');
+    detail.innerHTML = `
+  
         <div class="popup-title">${title}</div>
             <div class="popup-date">${date}</div>
             <div class="popup-time">${time}</div>
             <div class="popup-description">${description}</div>
     `;
-    fragment1.appendChild(div2);
-    requestAnimationFrame(() => {
-        popup.appendChild(fragment1);
-    })
+
 }
-
-
-
-// function tileDetail(e) {
-//     alert(e);
-//     let div1 = document.createElement('div'); 
-//     div1.setAttribute('class', 'modal');
-//     div1.innerHTML = `
-//     <div class="display-modal">
-//                 <button class="close">
-//                     <i class="bi bi-x-lg"></i>
-//                 </button>
-//                 <form class="form">
-//                     <div class="title">
-//                         <h4>Title</h4>
-//                         <input type="text" class="title-value">
-//                     </div>
-//                     <div class="description">
-//                         <h4>Description</h4>
-//                         <textarea name="description-text" class="description-text" maxlength="200"></textarea>
-//                     </div>
-//                     <div class="date">
-//                         <h4>Date</h4>
-//                         <input type="datetime-local" class="date-time">
-//                     </div>
-//                 </form>`;
-//     body.appendChild(div1);
-// }
 
 
 
@@ -108,10 +105,11 @@ function display() {
     })
     // console.log(tasks);
         let fragment = document.createDocumentFragment();
-    let taskDisplay = document.querySelector('.tasks');
+
     taskDisplay.style.display = "grid";
+    taskDisplay.style.gap = "2rem";
     taskDisplay.style.gridTemplateColumns = "repeat(5, 1fr)";
-    taskDisplay.style.padding = "1rem";
+    taskDisplay.style.padding = "2rem";
     // taskDisplay.style.border = "1px solid black";
     taskDisplay.style.height = "4rem";
     taskDisplay.innerHTML = ``;
